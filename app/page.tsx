613171af6d27d2ec90b9f599185965ed74f224dd
adiputha <adithyaperera456@gmail.com>
2024-07-21 11:30:45 +0530

    small changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Check, Star } from "lucide-react";
 import Phone from "@/components/Phone"; // Assuming Phone is a custom component
+import { Icons } from "@/components/Icons";
 
 export default function Home() {
   return (
@@ -20,12 +21,15 @@ export default function Home() {
                 />
               </div>
               <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
-                Your Image on a <span className="bg-green-600 px-2 text-white">Custom</span> Phone Case
+                Your Image on a{" "}
+                <span className="bg-green-600 px-2 text-white">Custom</span>{" "}
+                Phone Case
               </h1>
               <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
                 Capture your favorite memories with your own,{" "}
                 <span className="font-semibold">one-of-one</span> Phone case.
-                CaseCustom allows you to protect your memories, not just your phone case.
+                CaseCustom allows you to protect your memories, not just your
+                phone case.
               </p>
 
               <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
@@ -35,8 +39,8 @@ export default function Home() {
                     High-quality, durable material
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
-                    <Check className="h-5 w-5 shrink-0 text-green-600" />
-                    5 year print guaranteed
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />5 year
+                    print guaranteed
                   </li>
                   <li className="flex gap-1.5 items-center text-left">
                     <Check className="h-5 w-5 shrink-0 text-green-600" />
@@ -46,7 +50,13 @@ export default function Home() {
               </ul>
               <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
                 <div className="flex -space-x-4">
-                  {["/users/user-1.png", "/users/user-2.png", "/users/user-3.png", "/users/user-4.jpg", "/users/user-5.jpg"].map((src, index) => (
+                  {[
+                    "/users/user-1.png",
+                    "/users/user-2.png",
+                    "/users/user-3.png",
+                    "/users/user-4.jpg",
+                    "/users/user-5.jpg",
+                  ].map((src, index) => (
                     <Image
                       key={index}
                       src={src}
@@ -60,7 +70,10 @@ export default function Home() {
                 <div className="flex flex-col justify-between items-center sm:items-start">
                   <div className="flex gap-0.5">
                     {[...Array(5)].map((_, index) => (
-                      <Star key={index} className="h-4 w-4 text-green-600 fill-green-600" />
+                      <Star
+                        key={index}
+                        className="h-4 w-4 text-green-600 fill-green-600"
+                      />
                     ))}
                   </div>
                   <p>
@@ -90,6 +103,97 @@ export default function Home() {
               <Phone className="w-64" imgSrc="/testimonials/1.jpg">
                 {/* check the photo why it is not loading */}
               </Phone>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+
+      {/* Value proposition sec*/}
+      <section className="bg-slate-100 py-24">
+        <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+          <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+            <h2
+              className="order-1 mt-2 tracking-tight text-center text-balance 
+            !leading-tight font-bold text-5xl md:text-6xl
+            text-gray-900"
+            >
+              What our{" "}
+              <span className="relative px-2">
+                customers
+                <Icons.underline
+                  className="hidden sm:block pointer-events-none absolute inset-x-0
+                -bottom-6 text-green-500"
+                />
+              </span>{" "}
+              say
+            </h2>
+            <img src="/snake-2.png" className="w-24 order-0 lg:order-2" />
+          </div>
+
+          <div
+            className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2
+          gap-y-16"
+          >
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "I love my new case! The quality is amazing and the print is
+                  perfect. I'm so happy with my purchase. Thank you!"
+                </p>
+              </div>
+              <div className="flext gap-4 mt-2">
+                <img
+                  src="/users/user-1.png"
+                  className="rounded-full h-12 w-12 object-cover"
+                  alt="user"
+                />
+
+                <div className="flex flex-col">
+                  <p className="font-semibold">Chandrapala</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 strole-[3px] text-green-600" />
+                    <p className="text-sm">Verified Buyer</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  "I recently purchased a simple phone case for my iPhone, and I
+                  am thoroughly impressed with its quality and functionality.
+                  Here are a few key points about why I love this case."
+                </p>
+              </div>
+              <div className="flext gap-4 mt-2">
+                <img
+                  src="/users/user-2.png"
+                  className="rounded-full h-12 w-12 object-cover"
+                  alt="user"
+                />
+
+                <div className="flex flex-col">
+                  <p className="font-semibold">Mohini</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="h-4 w-4 strole-[3px] text-green-600" />
+                    <p className="text-sm">Verified Buyer</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </MaxWidthWrapper>
